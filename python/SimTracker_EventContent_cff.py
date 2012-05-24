8074a96a9e3e58b86d303317d24c8b187387be31
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-05-24 21:04:57 +0000

    Initial changes for pileup memory reduction

--- python/SimTracker_EventContent_cff.py
+++ python/SimTracker_EventContent_cff.py
@@ -7,7 +7,6 @@ SimTrackerFEVTDEBUG = cms.PSet(
         'keep *_simSiStripDigis_*_*', 
         'drop *_mix_simSiPixelDigis*_*', 
         'drop *_mix_simSiStripDigis*_*', 
-        'keep *_digiSimLink_*_*', 
         'keep *_allTrackMCMatch_*_*', 
         'keep *_trackingParticleRecoTrackAsssociation_*_*', 
         'keep *_assoc2secStepTk_*_*', 
@@ -19,8 +18,10 @@ SimTrackerFEVTDEBUG = cms.PSet(
 
 SimTrackerDEBUG = cms.PSet(
     outputCommands = cms.untracked.vstring(
-        'keep PixelDigiSimLinkedmDetSetVector_mix_simSiPixelDigis*_*', 
-        'keep StripDigiSimLinkedmDetSetVector_digiSimLink_*_*', 
+        'keep PixelDigiSimLinkedmDetSetVector_simSiPixelDigis_*_*', 
+        'keep StripDigiSimLinkedmDetSetVector_simSiStripDigis_*_*', 
+        'drop *_mix_simSiPixelDigis*_*', 
+        'drop *_mix_simSiStripDigis*_*', 
         'keep *_allTrackMCMatch_*_*')
 )
 #RAW content 
--- python/SimTracker_cff.py
+++ python/SimTracker_cff.py
@@ -1,6 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
+from SimTracker.SiStripDigitizer.SiStripDigiSimLink_cfi import simSiStripDigiSimLink
+
 # Combined sequence
-trDigi = cms.Sequence()
+trDigi = cms.Sequence(simSiStripDigiSimLink)
 
 import SimGeneral.MixingModule.stripDigitizer_APVModeDec_cff 
